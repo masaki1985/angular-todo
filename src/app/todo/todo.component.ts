53eaaee58b4900b0fc594ab84b526bc705ba0f5e
masaki <masaki.iima@gmail.com>
2017-08-19 16:59:37 +0900

    feat:todoリスト

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  TodoList = [
+    // { todo : '宿題', isCompleted: false, isEdit: false },
+    // { todo : '昼寝', isCompleted: false, isEdit: false },
+    // { todo : '食事', isCompleted: false, isEdit: false }
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(element) {
+    const data = { todo: element.value, isCompleted: false, isEdit: false}
+    this.TodoList.push(data);
+    element.value = '';
+  }
+
+  doCheck(index) {
+    if (this.TodoList[index].isCompleted) {
+      this.TodoList[index].isCompleted = false;
+    } else {
+      this.TodoList[index].isCompleted = true;
+    }
+  }
+
+  delete(index) {
+    if (!this.TodoList[index].isCompleted) { return; }
+    this.TodoList.splice(index, 1);
+  }
+
+  edit(index) {
+    this.TodoList[index].isEdit = true;
+  }
+
+  update(index, value) {
+    this.TodoList[index].todo = value;
+    this.TodoList[index].isEdit = false;
+  }
 }
+
+
